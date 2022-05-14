e51305448f340f8abf1eb6e5fda724060d3f5aa3
Romsh Kirtha <samuel.umek@gmail.com>
2022-05-14 23:40:26 +0200

    Enter key event, factorisation and getCoromonName

--- js/coromon-location.js
+++ js/coromon-location.js
@@ -5,22 +5,18 @@
 
 $(document).ready(function(){
     $(".reset").click(function(){
-        $(".responseModal").css("display","none");
-        $(".inputCoromon").val("");
-        $(".spriteNormal").attr("src","");
-        $(".spritePotent").attr("src","");
-        $(".spritePerfect").attr("src","");
-        $(".idCoromon").text("");
-        $(".nameCoromon").text("");
-        $(".wikiCoromon a").attr("href","");
-        $(".locationList ul")[0].innerHTML = "";
+        reset("all");
     });
 
     $(".submit").click(function(){
-        var coromonInput = inputReplace($(".inputCoromon").val());
-        var coromonID = getCoromonID(capitalizeFirstLetter(coromonInput));
-        var l = getLocationsByCoromonID(coromonID);
+        getCoromon();
     });
+
+    $(".inputCoromon").on('keypress', function(e){
+        if(e.which == 13){
+            getCoromon();
+        }
+    })
 
     COROMONDATA = {
         encounterList:getEncounterListPromise(),
@@ -28,6 +24,44 @@ $(document).ready(function(){
     }
 });
 
+function reset(all){
+    $(".responseModal").css("display","none");
+    $("#outerError").css("display","none");
+    $(".spriteNormal").attr("src","");
+    $(".spritePotent").attr("src","");
+    $(".spritePerfect").attr("src","");
+    $(".idCoromon").text("");
+    $(".nameCoromon").text("");
+    $(".wikiCoromon a").attr("href","");
+    $(".locationList ul")[0].innerHTML = "";
+
+    switch(all){
+        case "all":
+            $(".inputCoromon").val("");
+            break;
+    }
+}
+
+function getCoromon(){
+    var coromonInput = inputReplace($(".inputCoromon").val());
+    var coromonID = getCoromonID(capitalizeFirstLetter(coromonInput));
+
+    if(coromonID != null){
+        $("#outerError").css("display","none");
+        $(".responseModal").css("display","block");
+        var l = getLocationsByCoromonID(coromonID);
+        console.log(coromonID);
+        console.log(l);
+        setResponseModale(coromonID,l);
+    }else{
+        reset(1);
+        $(".responseModal").css("display","none");
+        $("#outerError").css("display","block");
+        $(".errorModal span").text("Error : Coromon not found");
+    }
+}
+
+//Function needed to stop js injection attacks
 function inputReplace(coromonInput){
     return coromonInput.replace(/</g,"&lt;").replace(/>/g,"&gt;");
 }
@@ -45,6 +79,7 @@ function getCoromonToBdd(){
     fetch("./data/CoromonToBDD.json").then(res=>res.json()).then(data => (COROMONDATA.coromonToBdd = data));;
 }
 
+
 function getCoromonID(coromonName){
     var coromons = COROMONDATA.coromonToBdd.coromons;
     for(key in coromons){
@@ -56,22 +91,16 @@ function getCoromonID(coromonName){
 }
 
 function getCoromonName(coromonID){
-
+    var coromons = COROMONDATA.coromonToBdd.coromons;
+    for(key in coromons){
+        if(key == coromonID){
+            return coromons[key];
+        }
+    }
+    return null;
 }
 
 function getLocationsByCoromonID(coromonID){
-    /*
-        Choses à modifier :
-            .idCoromon => coromonID
-            .nameCoromon =>  getCoromonName(coromonID)
-            .wikiCoromon.href => https://coromon.wiki.gg/wiki/getCoromonName(coromonID)
-
-        .title span => Place
-        .idLocation => subPlace
-        .idRate => currProba/probaTotal
-        .idName => description de la zone, à laisser pour le moment, il faut que je fasse un json IDLocation => Description
-        .idPosition => 1st, 2nd or 3rd position in an encounter
-    */
     var res = [];
     //place is the location
     for(place in COROMONDATA.encounterList){
@@ -122,4 +151,47 @@ function getLocationsByCoromonID(coromonID){
         }
     }
     return res;
+}
+
+function setResponseModale(coromonID, encounterList){
+    /*
+        Choses à modifier :
+            .idCoromon => coromonID
+            .nameCoromon =>  getCoromonName(coromonID)
+            .wikiCoromon.href => https://coromon.wiki.gg/wiki/getCoromonName(coromonID)
+
+        .title span => Place
+        .idLocation => subPlace
+        .idRate => currProba/probaTotal
+        .idName => description de la zone, à laisser pour le moment, il faut que je fasse un json IDLocation => Description
+        .idPosition => 1st, 2nd or 3rd position in an encounter
+    */
+
+    let coromonName = getCoromonName(coromonID);
+    let ul = $(".locationList ul");
+    ul.empty();
+
+    //idCard
+    $(".coromonNormal img").attr("src","./images/Coromon Sprites/"+coromonName+"_normal.gif");
+    $(".coromonPotent img").attr("src","./images/Coromon Sprites/"+coromonName+"_potent.gif");
+    $(".coromonPerfect img").attr("src","./images/Coromon Sprites/"+coromonName+"_perfect.gif");
+    $(".idCoromon").text(coromonID);
+    $(".nameCoromon").text(coromonName);
+    $(".wikiCoromon a").attr("href","https://coromon.wiki.gg/wiki/"+coromonName);
+
+    let res = "<li class=\"title\">\n";
+    res += "<span>Test1</span>\n";    //Place
+    res += "</li>\n";
+    res += "<li><div class=\"content\">\n";
+    res += "<label for=\"idLocation\">ID :</label>\n";
+    res += "<span name=\"idLocation\" class=\"idLocation\">sub test</span>\n";  //subPlace
+    res += "<label for=\"idRate\">Rate :</label>\n";
+    res += "<span name=\"idRate\" class=\"idRate\">33%</span><br>\n"; // currProba/probaTotal to be calculate
+    res += "<label for=\"idName\">Name :</label>\n";
+    res += "<span name=\"idName\" class=\"idName\">Wesh comment ça va</span>\n";    // To be modify afterward
+    res += "<label class=\"labelPosition\" for=\"idPosition\">Horde :</label>\n";
+    res += "<span name=\"idPosition\" class=\"idPosition\">1</span>\n";    //Position
+    res += "</div></li>"
+
+    //$(res).appendTo(ul);
 }
